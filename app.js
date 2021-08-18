cdba4675df2440e631f281f5355004ff1fe16cf7
AntonBereza1995 <80589071+AntonBereza1995@users.noreply.github.com>
2021-08-18 10:54:22 +0300

    подгрузил

---
+++ app.js
@@ -0,0 +1,134 @@
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+
+
+const createGallery = document.querySelector(".js-gallery");
+const imagesGallery = createGalleryItems(galleryItems);
+
+createGallery.insertAdjacentHTML("beforeend", imagesGallery);
+
+
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class = "gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+
+}
+
+// function onGallaryClick(evt) {
+ 
+//   const isGallaryImages = evt.target.contains("gallery__image");
+//   if (!isGallaryImages) {
+//     return;
+//   }
+
+// }
+
+const modal = document.querySelector(".js-lightbox");
+const isOpenModalImg = document.querySelector(".lightbox__image");
+const closeBtn = document.querySelector(".lightbox__button");
+const overlay = document.querySelector(".lightbox__overlay");
+
+function openModal(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+const watchImages = evt.target;
+const orgImg = watchImages.dataset.source;
+  isOpenModalImg.src = orgImg;
+  isOpenModalImg.alt = watchImages.alt;
+  isOpenModalImg.dataset.index = watchImages.dataset.index;
+  modal.classList.add("is-open");
+};
+createGallery.addEventListener("click", openModal)
+
+function closeModal(evt) {
+  isOpenModalImg.src = " ";
+  isOpenModalImg.alt = " ";
+  isOpenModalImg.dataset.index = " ";
+  modal.classList.remove("is-open")
+};
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
